cafb85db37a929c3065cd9ee704b075128bd078d
Phoebe <phoebesimhn@gmail.com>
2025-08-02 09:59:37 +0800

    Initial Commit

---
+++ Stickman/StickmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickmanApp.swift
+//  Stickman
+//
+//  Created by Phoebe Sim on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StickmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
